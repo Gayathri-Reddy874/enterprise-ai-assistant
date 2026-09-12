f369e0b4c4c2d1bdaeae4524f8617713b41b9978
Mallareddygari Gayathri <gayathrireddy18125@gmail.com>
2026-09-12 09:10:06 +0530

    Remove Memory class from memory.py
    Remove Memory class and its methods for handling JSON data.

--- core/memory.py
+++ core/memory.py
@@ -1,16 +1,1 @@
-import json
 
-class Memory:
-    def __init__(self):
-        self.file = "memory.json"
-        try:
-            self.data = json.load(open(self.file))
-        except:
-            self.data = []
-
-    def save(self, query, response):
-        self.data.append({"q": query, "r": response})
-        json.dump(self.data, open(self.file, "w"))
-
-    def context(self):
-        return self.data[-5:]
